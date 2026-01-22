479a4e2791d2962e9256e3efa94eda70d6e3eb82
wangyukunZoe <wangyukun20011027@icloud.com>
2026-01-23 02:25:04 +0800

    Hoc Hook
    Hoc Hook

--- react/react-hooks/src/App.js
+++ react/react-hooks/src/App.js
@@ -4,20 +4,36 @@ import LikeButton from './components/LikeButton';
 import MouseTracker from './components/MouseTracker';
 import DogShow from './components/DogShow';
 import useMousePosition from './hooks/useMousePosition';
+import withLoader from './components/withLoader';
+
+// 不建议使用的 react 高级属性  //也很难实现再次请求图片的操作
+const DogShowLoader = ({ isLoading, data }) => {
+  const style = {
+    width: 200
+  }
+  return (
+    <>
+      {isLoading ? <p>🐶Loading ... </p>
+        : <img src={data.message} alt="dog" style={style} />
+      }
+    </>
+  )
+}
 
 function App() {
-
-  const position = useMousePosition();
+  // const position = useMousePosition();
+  const DogShowWithLoader = withLoader(DogShowLoader, "https://dog.ceo/api/breeds/image/random")
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <h1>{position.x}</h1>
-        <MouseTracker />
+        {/* <h1>{position.x}</h1> */}
+        {/* <MouseTracker /> */}
 
         <DogShow />
+        <DogShowWithLoader />
         <LikeButton />
         <a
           className="App-link"
---
+++ react/react-hooks/src/components/withLoader.jsx
@@ -0,0 +1,39 @@
+// high order component
+import React from 'react'
+import axios from 'axios'
+
+//HOC react 高级特性
+const withLoader = (WrappedComponent, url) => {
+  return class LoaderComponent extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        data: null,
+        isLoading: false
+      }
+    }
+    componentDidMount() {
+      this.setState({
+        isLoading: true,
+      })
+      axios.get(url).then(result => {
+        this.setState({
+          data: result.data,
+          isLoading: false
+        })
+      })
+    }
+    render() {
+      const { data, isLoading } = this.state
+      return (
+        <>
+          { (isLoading || !data) ? <p>data is loading</p> :
+            <WrappedComponent {...this.props} data={data} isLoading={isLoading} />
+          }
+        </>
+      )
+    }
+  }
+}
+
+export default withLoader
